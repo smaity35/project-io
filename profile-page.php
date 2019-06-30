aad97e57ff2eef3ea48da6faed9fc9179e8fdf78
unknown <smaity35@gmail.com>
2019-06-30 21:57:39 +0530

    Work Update

--- profile-page.php
+++ profile-page.php
@@ -23,11 +23,23 @@
 
         </div>
         <div class="tab-content">
-          <div id="menu0" class="tab-pane fade in active">
-            
+          <div id="menu0" class="tab-pane fade in active">            
 
             <div class="row">
               <div class="col-md-3">
+                <div class="title sec-9">
+                  <h2>6,530</h2>
+                  <p>patrons</p>
+                  <div class="btn-wrap">
+                    <button><i class="ion ion-md-add"></i> Follow</button>
+                    <button><i class="ion ion-md-share-alt"></i> Share</button>
+                  </div>
+                  <ul class="social-links">
+                    <li><a href=""><i class="ion ion-logo-facebook"></i></a></li>
+                    <li><a href=""><i class="ion ion-logo-twitter"></i></a></li>
+                    <li><a href=""><i class="ion ion-logo-youtube"></i></a></li>
+                  </ul>
+                </div>
               </div>
               <div class="col-md-6">
                   <div class="title overview-tile1">
@@ -114,56 +126,172 @@
                   
               </div>
               <div class="col-md-3">
-                  <div class="big-btn">
+                  <!-- <div class="big-btn">
                       <button class="active" disabled>Save Changes</button>
-                  </div>
+                  </div> -->
                   <div class="title sec-5">
-                      <h6>CHECKLIST</h6>
-                      <ul class="list-type-1">
-                          <li class="green">
-                              <div class="round done"><i class="ion ion-md-checkmark"></i></div>
-                              <div class="content">
-                                  <p>Upload profile picture</p>
-                                  <small>Required <a href="#">add now</a></small>
-                              </div>
-                          </li>
-                          <li class="red">
-                              <div class="round"><i class="ion ion-md-checkmark"></i></div>
-                              <div class="content">
-                                  <p>Upload cover image</p>
-                                  <small>Required <a href="#">add now</a></small>
-                              </div>
-                          </li>
-                          <li class="red">
-                              <div class="round"><i class="ion ion-md-checkmark"></i></div>
-                              <div class="content">
-                                  <p>Create about section</p>
-                                  <small>Required <a href="#">add now</a></small>
-                              </div>
-                          </li>
-                          <li class="red">
-                              <div class="round"><i class="ion ion-md-checkmark"></i></div>
-                              <div class="content">
-                                  <p>Verify phone number</p>
-                                  <small>Required <a href="#">add now</a></small>
-                              </div>
-                          </li>
-                      </ul>
-                      <h6>LEARN MORE</h6>
+                      <h6>TIERS</h6>
+                      <hr>
+                      <h4>Get all the updates</h4>
+                      <p>$3 or more per month</p>
+
                       <ul class="list-type-2">
-                          <li><a href="">Membership 101: Best Practices</a></li>
-                          <li><a href="">How to choose your business model</a></li>
-                          <li><a href="">How to talk about Patreon to your audience</a></li>
-                          <li><a href="">Knowing your worth as a creator</a></li>
+                          <li><a href="">Blog posts and updates</a></li>
+                          <li><a href="">Occasional shaky VLOGs</a></li>
+                          <li><a href="">A tiny gift in the mail once a year-ish</a></li>
                       </ul>
+                      <div class="link"><a href="">Join $3 Tier</a></div>
+                      <hr>
+                      <h4>All the updates plus extra glory</h4>
+                      <p>$5 or more per month</p>
+                      <ul class="list-type-2">
+                          <li><a href="">Blog posts and updates</a></li>
+                          <li><a href="">Occasional shaky VLOGs</a></li>
+                          <li><a href="">A tiny gift in the mail once a year-ish</a></li>
+                          <li><a href="">Putting your name in the jar for giveaways</a></li>
+                          <li><a href="">Eternal glory</a></li>
+                      </ul>
+                      <div class="link"><a href="">Join $5 Tier</a></div>
                   </div>
               </div>
             </div>
 
           </div>
           <div id="menu1" class="tab-pane fade">
-            
-           
+            <div class="row">
+              <div class="col-md-3">
+                <div class="title sec-10">
+                  <h3>FILTER  <a href="">All Posts</a></h3>
+                  <hr>
+
+                  <div class="filter-list">
+                    <select>
+                      <option hidden value="Filter by tier" >Filter by tier</option>
+                      <option hidden value="All Patrons (106)" >All Patrons (106)</option>
+                      <?php
+                          for($i=1; $i<=10; $i++){
+                      ?>
+                        <option value="Public (6)">Public (6)</option>
+                      <?php } ?>
+                    </select>
+                  </div>
+
+                  <div class="filter-list">
+                    <input type="date" name="" placeholder="Filter by month">
+                  </div>
+
+                  <div class="filter-list">
+                    <select>
+                      <option hidden value="Sort by date, newest first">Sort by date, newest first</option>
+                      <?php
+                          for($i=1; $i<=6; $i++){
+                      ?>
+                      <option value="Newest first">Newest first</option>
+                      <?php } ?>
+                    </select>
+                  </div>
+
+                </div>
+              </div>
+              <div class="col-md-6">
+    
+
+                  <div class="title overview-tile2">
+                    <table>
+                        <tr>
+
+                            <td>
+                                <i class="material-icons">lock_open</i>
+                            </td>
+
+                            <td>
+                                <p>
+                                  See more today! If you <a href="">become a patron</a> to <b>Issa Rae Presents</b>, you’ll immediately get access to as many as 99 patron-only posts.
+                                </p>
+                            </td>
+
+                        </tr>
+                        
+                    </table>
+                  </div>
+                  <?php
+                      for($i=1; $i<=25; $i++){
+                  ?>
+                  <div class="title overview-tile3">
+                     <div class="imag-area" >
+                       <a href="" style="background-image: url('images/img4.jpg')">
+                         <div>
+                           <i class="material-icons">lock</i>
+                           <p>Unlock this <br>by becoming a patron</p>
+                           <button>Join now </button>
+                         </div>
+                       </a>
+                     </div>
+                     <div class="content-area">
+                       <div class="date-lock">
+                         <span><a href="">Apr 16 at 8:31am</a></span>
+                         <div class="lock"><span><i class="material-icons">lock</i> Locked</span>
+                          <div class="loc-status">
+                           <h4>Who can see this post</h4>
+                           <ul>
+                             <li>Exclusive IRP Patron</li>
+                             <li>Premier IRP Patron</li>
+                           </ul>
+                         
+                         </div>
+                         </div class="lock">
+                         
+                       </div>
+                       <div class="post-title">
+                         <a href="">Color Creative Masterclass with James Bland: The Giant of Momentum (Episode 4)</a>
+                       </div>
+                       <div class="class-tag">
+                        <a href=""><i class="material-icons">turned_in_not</i>
+                         Master Class</a>
+                       </div>
+                       <div class="comment-like">
+                         <div class="comment"><a href="">2 Comments</a></div>
+                         <div class="like"><span>6 Likes</span>
+                          <div class="cont">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                          </div>
+                         </div>
+                       </div>
+                     </div>
+                  </div>
+                  <?php } ?>
+                  
+              </div>
+              <div class="col-md-3">
+                  <!-- <div class="big-btn">
+                      <button class="active" disabled>Save Changes</button>
+                  </div> -->
+                  <div class="title sec-5">
+                      <h6>TIERS</h6>
+                      <hr>
+                      <h4>Get all the updates</h4>
+                      <p>$3 or more per month</p>
+
+                      <ul class="list-type-2">
+                          <li><a href="">Blog posts and updates</a></li>
+                          <li><a href="">Occasional shaky VLOGs</a></li>
+                          <li><a href="">A tiny gift in the mail once a year-ish</a></li>
+                      </ul>
+                      <div class="link"><a href="">Join $3 Tier</a></div>
+                      <hr>
+                      <h4>All the updates plus extra glory</h4>
+                      <p>$5 or more per month</p>
+                      <ul class="list-type-2">
+                          <li><a href="">Blog posts and updates</a></li>
+                          <li><a href="">Occasional shaky VLOGs</a></li>
+                          <li><a href="">A tiny gift in the mail once a year-ish</a></li>
+                          <li><a href="">Putting your name in the jar for giveaways</a></li>
+                          <li><a href="">Eternal glory</a></li>
+                      </ul>
+                      <div class="link"><a href="">Join $5 Tier</a></div>
+                  </div>
+              </div>
+            </div>
           </div>
        
           
